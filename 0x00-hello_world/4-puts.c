2778a2406190fa3eedf80a94ad65cbae9fc4671a
Mrsave-tech <joelsave0@gmail.com>
2022-12-09 15:46:57 +0100

    well its done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/*
-* main means entry
-*Always return 0*/
+/**
+* main - Entry
+*
+* Return: Always 0
+*/
 
-int main (void)
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
-	}
+		puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
