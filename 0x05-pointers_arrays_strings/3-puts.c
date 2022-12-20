e0e0bc0c8190dcfc5a961e3c3d04596472cab461
Mrsave-tech <joelsave0@gmail.com>
2022-12-20 23:45:24 +0100

    realease

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * main - check the code
+ *  _puts - Write a function that prints a string, followed by a new line
  *
- * Return: Always 0.
+ *  @str: This is input string
+ *  Description: Write a function that prints a string
+ *
+ *  Return: a string.
  */
-int main(void)
+void _puts(char *str)
 {
-	char *str;
+	int i;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_puts(str);
-	return (0);
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
