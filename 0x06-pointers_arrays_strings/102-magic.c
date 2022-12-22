33b900c1a80898592f57a91191c6b364532bea65
Mrsave-tech <joelsave0@gmail.com>
2022-12-22 16:43:26 +0100

    function that prints a buffer

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - entry point
+ * Return: Always 0
  */
 int main(void)
 {
@@ -9,7 +10,6 @@ int main(void)
 	int *p;
 		p = &n;
 	/*
-	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not alloweed to modify p
