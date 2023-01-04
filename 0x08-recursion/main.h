9b6becf0fbce44c693a396dbe7455aa6c2e18c7a
Mrsave-tech <joelsave0@gmail.com>
2023-01-04 23:08:43 +0100

    messages

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,8 @@
  *       used in the 0x07-recursion directory.
  */
 
-int _putchar(char c);
+
+int _putchar(char c); 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -16,9 +17,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-
-#endif
 
 #endif /* MAIN_H_ */
