437230b7e622b78d2eae86c324eee3dca3fc94a3
Mrsave-tech <joelsave0@gmail.com>
2023-05-22 00:39:57 +0100

    positive and negative with decision statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
@@ -12,9 +13,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
+	if (n > 0)
 		printf("the number is greter than 0 thus, number is positive");
-	else if
+	else if (n < 0)
 		printf("the number is less than 0 thus, number is negative");
 	else
 		printf("the number is 0");
