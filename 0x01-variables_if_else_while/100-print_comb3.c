520ff0ca65ebcf0dbf06bf8240e4390848834d84
Mrsave-tech <joelsave0@gmail.com>
2023-05-22 23:40:00 +0100

    combination of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 	{
-		for (num1 = 0; num < 10; num++)
+		for (num1 = 0; num1 < 10; num1++)
 		{
 			putchar((num % 10) + '0');
 			putchar((num1 % 10) + '0');
