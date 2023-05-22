db908ddb01211cec304611aed70c1766c1625cc6
Mrsave-tech <joelsave0@gmail.com>
2023-05-22 23:21:10 +0100

    combination of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ *
+ * Return: Always zero
+ * author: Mr Save
+ */
+int main(void)
+{
+	int num, num1;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (num = 0; num < 10; num++)
+		{
+			putchar((num % 10) + '0');
+			putchar((num % 10) + '0');
+
+			if (num == 9 && num == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
