14c7c7ce12e51f075383311aa2d5f6aac5fa2329
Mrsave-tech <joelsave0@gmail.com>
2023-05-22 14:16:50 +0100

    print number base 10 starting from zero

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always Zero
+ * Author: Mr Save
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d\n", num);
+	return (0);
+}
