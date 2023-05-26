848e77fe21dafda6fb7d380b5cbd1f589260fa08
Mrsave-tech <joelsave0@gmail.com>
2023-05-26 02:48:51 +0100

    print lower case alpahabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ *
+ * Return: void
+ * Author: Mr save
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			ch++;
+		}
+		i++;
+	}
