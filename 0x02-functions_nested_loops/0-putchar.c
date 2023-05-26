a7bb7eb75b01ddba3ab18a3c9ddc766fb52c1081
Mrsave-tech <joelsave0@gmail.com>
2023-05-26 01:40:22 +0100

    printing putchar using main.h as headwe file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - Entry
+ *
+ * Return: Zero for this program
+ * Author: Mr save
+ */
+int _putchar(char c)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
+
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C functions, header files and nested loops
