f575ca24fd5f8eb9f8606a52cafd962d9fd6b0fb
Mrsave-tech <joelsave0@gmail.com>
2023-05-26 02:01:17 +0100

    prints alaphabet in lower case using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - starting point
+ *
+ * Description: prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0.
+ * Author: Mr Save
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for(ch = a; ch <= z; ch++);
+	_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
